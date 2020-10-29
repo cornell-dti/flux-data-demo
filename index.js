4e97f0f3cc25b7e14e930a05f80f729108561152
Andrew <kungpaogao@gmail.com>
2020-10-29 14:01:35 -0400

    Add more docs for JS

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+/** Simulation values */
 let time = 0;
-let maxStep = 7;
-
-let timer = null;
 let queue = [];
 
+/** Variable that holds the timer interval */
+let timer = null;
+
+/** Input values */
 let speed = 1;
 let incoming = 1;
 let serving = 5;
 
-// source: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+/**
+ * Generates a unique ID
+ * Source: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+ * @param {number} length - length of ID
+ */
 function makeId(length) {
   var result = "";
   var characters =
@@ -20,6 +26,10 @@ function makeId(length) {
   return result;
 }
 
+/**
+ * Updates speed data to match input
+ * @param {string} value
+ */
 function updateSpeed(value) {
   speed = parseFloat(value);
   document.getElementById("speed-display").innerHTML = speed;
@@ -27,14 +37,26 @@ function updateSpeed(value) {
   start();
 }
 
+/**
+ * Updates incoming data to match input
+ * @param {string} value
+ */
 function updateIncoming(value) {
   incoming = parseInt(value);
 }
 
+/**
+ * Updates serving data to match input
+ * @param {string} value
+ */
 function updateServing(value) {
   serving = parseInt(value);
 }
 
+/**
+ * Returns color associated with counter
+ * @param {number} counter - current value of box
+ */
 function getBoxColor(counter) {
   const value = counter / serving;
   if (value >= 0.75) return "#f56565";
@@ -44,8 +66,8 @@ function getBoxColor(counter) {
 }
 
 /**
- * Renders all the elements to reflect the current data
- **/
+ * Renders UI elements based on current data
+ */
 function render() {
   document.getElementById("time").innerHTML = time;
   document.getElementById("queue").innerHTML = JSON.stringify(queue, null, 2);
@@ -69,23 +91,40 @@ function render() {
   document.getElementById("waiting").innerHTML = getWaitTime();
 }
 
-// https://www.desmos.com/calculator/dxehrz5pe4
+/**
+ * Returns `f(x)` based on a Gaussian function with parameters a, b, c
+ * Read more: https://en.wikipedia.org/wiki/Gaussian_function
+ * Visualization: https://www.desmos.com/calculator/dxehrz5pe4
+ * @param {number} x - input variable
+ * @param {number} a - a in Gaussian function
+ * @param {number} b - b in Gaussian function
+ * @param {number} c - c in Gaussian function
+ */
 function gaussian(x, a = 33, b = 12, c = 1) {
   return a * Math.exp((-1 * (x - b) ** 2) / (2 * c ** 2));
 }
 
+/**
+ * Called at each interval iteration
+ */
 function step() {
-  // add to queue
+  // add to queue for each incoming person
   for (let i = 0; i < incoming; i++) {
     queue.push({ id: makeId(3), counter: serving });
   }
+
+  // update incoming rate according to Gaussian function
   incoming = gaussian(time) >= 0.01 ? gaussian(time) : 0;
   // console.log(time, incoming);
+
+  // update first person on queue by decrementing counter
   if (queue.length > 0) {
     // TODO: update to enable serving time to be function-based
     queue[0].counter -= 1;
     if (queue[0].counter === 0) queue = queue.slice(1);
   }
+
+  // update time and rerender
   time += 1;
   render();
 }
